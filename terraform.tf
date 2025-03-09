f4f5b143dd4ba07ad08c36833901202e25e30837
Julia <js@example.com>
2025-03-09 20:54:44 +0100

    Remove old module files and clean the set up

--- terraform.tf
+++ terraform.tf
@@ -3,23 +3,25 @@ terraform {
   required_providers {
     alz = {
       source  = "azure/alz"
-      version = "~> 0.17"
+      version = "~> 0.16"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "~> 2.2"
+      version = "~> 2.0"
     }
-    modtm = {
-      source  = "azure/modtm"
-      version = "~> 0.3"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  tenant_id       = "60484d41-f4c4-4a62-a76f-b056885aedd0"
+  subscription_id = "a8fd746f-85be-4734-a7b6-3e532a9bfa77"
 }
